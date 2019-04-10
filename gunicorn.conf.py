21726292d5ce35ff34a0a5be52a22c329daebdee
louis <yangzhikai@cug.edu.cn>
2019-04-10 15:43:57 +0800

    change gunicorn setting

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "0.0.0.0:8000"   #绑定的ip与端口
+bind = "127.0.0.1:8000"   #绑定的ip与端口
 workers = 4                #核心数
 errorlog = '/root/recruitKG/log/error.log' #发生错误时log的路径
 accesslog = '/root/recruitKG/log/sucess.log' #正常时的log路径
