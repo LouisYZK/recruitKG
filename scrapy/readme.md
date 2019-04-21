358f938013ab63e99b31600b27734cf245fe5072
louis <yangzhikai@cug.edu.cn>
2019-04-21 16:08:12 +0800

    处理数据库

---
+++ scrapy/readme.md
@@ -0,0 +1,9 @@
+# get postion data from zhilian recruite
+
+apply a multi-thread spider on some apis.
+
+run:
+
+```bash
+sh ./run.sh
+```
--- scrapy/test_data.py
+++ scrapy/test_data.py
@@ -1,11 +1,37 @@
 import sqlite3
-
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
 # query table info:
 conn = sqlite3.connect('zhilian.db')
 cur = conn.cursor()
+
+"""
+create new columns
+"""
 # cur.execute("PRAGMA table_info(zhilian)")
-# print(cur.fetchall())
-cur.execute('select * from zhilian')
-data = cur.fetchall()
-for item in data:
-    print(item[0])
+# cols = cur.fetchall()
+# cols_name = set([item[1] for item in cols])
+# if 'is_unique' not in cols_name:
+#     cur.execute('alter table zhilian add column is_unique int;')
+
+# 检查重复
+
+
+def check_unique():
+    data = next(gen)
+    name, position = data[1], data[7] 
+    if (name, position) not in seen:
+        seen.add((name, position))
+        print(name, position)
+
+with ThreadPoolExecutor(max_workers=4) as executor:
+    seen = set()
+    gen = cur.execute('select * from zhilian')
+    while True:
+        try:
+            executor.submit(check_unique())
+        except Exception as e:
+            print(e)
+            break
+        finally:
+            print('Unique num:', len(seen))
