73ee509529c3cf7d2fab0f57ae61f8fe87bcff9e
louis <yangzhikai@cug.edu.cn>
2019-04-21 19:03:47 +0800

    alter test

--- scrapy/test_data.py
+++ scrapy/test_data.py
@@ -19,7 +19,7 @@ create new columns
 
 def check_unique():
     data = next(gen)
-    name, position = data[0], data[1], data[7] 
+    name, position = data[1], data[7] 
     if (name, position) not in seen:
         seen.add((name, position))
         print(name, position)
