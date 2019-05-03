f73be37436b6f8b94d00dc8685b0a85700dcb36d
louis <yangzhikai@cug.edu.cn>
2019-05-03 15:11:52 +0800

    build graph

---
+++ scrapy/get_enti_and_know.py
@@ -0,0 +1,82 @@
+import sqlite3
+import requests
+import json
+
+"""
+Input: doc from zhilian_doc.db
+Aim:
+    get the entities/knowledges in the doc.
+    store them into entites.json/knowledges.json
+
+entities.json:
+{
+    'name+position':List(entities),
+}
+konwledges.json:
+{
+    'entity':[
+        ['relation', 'entity'],
+        ...
+    ],
+}
+"""
+def flatten(items):
+    for x in items:
+        if hasattr(x,'__iter__') and not isinstance(x, (str, bytes)):
+            # for sub_x in flatten(x):
+            #     yield sub_x
+            yield from flatten(x)
+        else:
+            yield x
+
+def get_entity(doc):
+    url = 'http://shuyantech.com/api/entitylinking/cutsegment'
+    doc = doc.split('。')
+    entities = []
+    for item in doc:
+        params = {'q':item}
+        r = requests.get(url, params=params)
+        entity = json.loads(r.text)['entities']
+        entities.append([item2[1] for item2 in entity])
+    return entities
+
+def get_triple_tuple(entities):
+    url = 'http://shuyantech.com/api/cndbpedia/avpair'
+    know = {}
+
+    for item in entities:
+        if item not in seen_entity:
+            seen_entity.add(item)
+            params = {'q':item}
+            text = requests.get(url, params=params).text
+            knowledge = json.loads(text)['ret']
+            know[item] = knowledge
+    return know
+
+def en_store_to_json(name, pos, entities):
+    en = {}
+    with open('./entities.json', 'a') as fp:
+        en[name + pos] = entities
+        json.dump(en, fp)
+
+def konw_store_to_json(name, pos, knows):
+    with open('./konws.json', 'a') as fp:
+        json.dump(knows, fp)
+
+conn = sqlite3.connect('zhilian_doc.db')
+cur = conn.cursor()
+data = cur.execute('select * from zhilian_doc')
+
+seen_entity = set()
+
+name, pos, doc = next(data)
+entities = get_entity(doc)
+
+while True:
+    name, pos, doc = next(data)
+    entities = get_entity(doc)
+    entities = list(flatten(entities))
+    knows = get_triple_tuple(entities)
+    print(entities)
+    en_store_to_json(name, pos, entities)
+    konw_store_to_json(name, pos, knows)
--- scrapy/readme.md
+++ scrapy/readme.md
@@ -7,3 +7,15 @@ run:
 ```bash
 sh ./run.sh
 ```
+
+jupyter数据科学环境终端部署：
+
+安装conda和jupyter之后，需要手动配置jupyter的多个kernel.
+
+```bash
+conda env create myenv --python=3.6
+source activate myenv
+conda isntall ipykernel
+python -m ipykernel install --user --name myenv --display-name "Python (myenv)" 
+```
+之后启动jupyter会自动绑定虚拟环境的kernel.
