d62d698ac9bee2cea58b2da84f4c33432aa7cb12
louis <yangzhikai@cug.edu.cn>
2019-05-04 14:54:10 +0800

    最终NER和关系提取版本

--- scrapy/get_enti_and_know_v2.py
+++ scrapy/get_enti_and_know_v2.py
@@ -2,7 +2,9 @@ import sqlite3
 import requests
 import json
 import time
+import itertools
 from process import clean_doc
+
 """
 Input: doc from zhilian_doc.db
 Aim:
@@ -53,6 +55,7 @@ def get_entity(doc):
     }
     enti = []
     for i in range(num):
+        time.sleep(1)
         chaxun = doc[i * 63: (i + 1)*63].strip()
         try:
             res = requests.post('https://aip.baidubce.com/rpc/2.0/kg/v1/cognitive/entity_annotation', 
@@ -66,21 +69,57 @@ def get_entity(doc):
             print('chauxn:', chaxun)
             continue
     return enti
-        
+
+def get_knows(entity):
+    url = 'http://zhishi.me/api/entity/'
+    if entity not in seen_en:
+        url = url + entity
+        try:
+            knows = requests.get(url, headers=headers).json()
+            baike = [k for k in knows.keys()]
+            info = [b for b in baike if 'infobox' in knows[b]]
+            print(info)
+            res_know = knows[info[0]]['infobox']
+            seen_en.add(entity)
+            return res_know
+        except Exception as e:
+            print(e)
+            return None
+
 def en_store_to_json(entities):
     with open('./entities.json', 'w') as fp:
         fp.truncate()
         json.dump(entities, fp)
+
+def know_store_to_json(knows):
+    """konows: {'en': {'relation': another_en}}
+    """
+    with open('./knows.json', 'w') as fp:
+        fp.truncate()
+        json.dump(knows, fp)
+
 
 conn = sqlite3.connect('zhilian_doc.db')
 cur = conn.cursor()
 data = cur.execute('select * from zhilian_doc')
 
 en = {}
-while True:
+knows = {}
+seen_en = set()
+
+tt = 0
+for t in itertools.count():
     name, pos, doc = next(data)
-    time.sleep(3)
     entities = get_entity(doc)
     print(entities)
+    print('..............第', t, '个岗位................')
     en[name + pos] = entities
     en_store_to_json(en)
+    for item in entities:
+        know = get_knows(item)
+        if know is not None:
+            knows[item] = know
+            know_store_to_json(knows)
+            tt += 1
+            print('......................收集到了.................', tt, '个实体的知识')
+            
