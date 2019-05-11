3ded0dfe16f5e04332ac2cae87071a4dfdfc34be
louis <yangzhikai@cug.edu.cn>
2019-05-11 15:10:19 +0800

    getv3持续采集

--- scrapy/get_enti_and_know_v3.py
+++ scrapy/get_enti_and_know_v3.py
@@ -156,9 +156,9 @@ if __name__ == '__main__':
     #         with open('null_return.pkl', 'wb') as fp:
     #             fp.truncate()
     #             pickle.dump(null_return_update, fp)
-    # with open('knows.json', 'r') as fp:
-    #     knows = json.load(fp)
-    knows = {}
+    with open('knows.json', 'r') as fp:
+        knows = json.load(fp)
+    # knows = {}
     for name, entities in ens.items():
         for en in entities:
             try:
